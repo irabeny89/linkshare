bf393048f59b57b4ed9ce092b4b3f85acdeb6ef8
Ernest Irabor <irabeny89@gmail.com>
2022-05-04 07:45:56 +0100

    add header component

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { MdShare } from "react-icons/md";
+import config from "config";
+import Link from "next/link";
+
+const {
+  siteData: { name, pages },
+} = config;
+
+export default function Header() {
+  return (
+    <header>
+      <Navbar collapseOnSelect expand="md">
+        <Navbar.Brand as="h1" className="text-danger">
+          <MdShare color="green" size={30} /> {name}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            {pages.map(({ href, title }) => (
+              <Nav.Item key={title} className="mx-3">
+                <Link passHref href={href}>
+                  {title}
+                </Link>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
+}
