f0587d5c550be3bdcfde734a8ab8851df5164eee
Ernest Irabor <irabeny89@gmail.com>
2022-05-04 08:21:30 +0100

    add web app config data

---
+++ config.ts
@@ -0,0 +1,18 @@
+const config = {
+  siteData: {
+    name: "LinkShare",
+    author: "Ernest Irabor",
+    pages: [
+      {
+        title: "Home",
+        href: "/",
+      },
+      {
+        title: "Member",
+        href: "/member"
+      }
+    ],
+  },
+};
+
+export default config;
