d0ce798917a7d5abcfd66de96dddb901250a7479
Ernest Irabor <irabeny89@gmail.com>
2022-05-04 10:42:09 +0100

    set up error pages and component

---
+++ components/Error.tsx
@@ -0,0 +1,18 @@
+import { Alert } from "react-bootstrap";
+import { MdError } from "react-icons/md";
+import config from "config";
+import { ErrorPropsType } from "types";
+
+export default function Error({ type }: ErrorPropsType) {
+  return (
+    <Alert variant="danger" className="border-3 text-center display-5">
+      <Alert.Heading>
+        {type === "404" && <>4xx - Client Error</>}
+        {type === "500" && <>5xx - Server Error</>}
+      </Alert.Heading>
+      {type === "404" && <p>{config.siteData.error.pageNotFound}</p>}
+      {type === "500" && <p>{config.siteData.error.serverDown}</p>}
+      <MdError size={100} />
+    </Alert>
+  );
+}
---
+++ pages/404.tsx
@@ -0,0 +1,5 @@
+import Error from "components/Error";
+
+export default function PageNotFoundErrorPage() {
+  return <Error type="404" />;
+}
