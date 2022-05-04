f11a0f0f83f456cfad01a1773590966f1e972c3e
Ernest Irabor <irabeny89@gmail.com>
2022-05-04 20:56:13 +0100

    add feed back toast component

---
+++ components/FeedBackToast.tsx
@@ -0,0 +1,28 @@
+import ToastContainer from "react-bootstrap/ToastContainer";
+import Toast from "react-bootstrap/Toast";
+import config from "config";
+import { FeedbackToastPropsType } from "types";
+import { useEffect } from "react";
+
+export default function FeedbackToast({
+  error,
+  show,
+  setShow,
+}: FeedbackToastPropsType) {
+  useEffect(() => {
+    setShow(!!error);
+  }, [error]);
+  
+  return (
+    <ToastContainer position="bottom-end">
+      <Toast bg="danger" autohide show={show} onClose={() => setShow(false)}>
+        <Toast.Header className="justify-content-between">
+          {error?.name}
+        </Toast.Header>
+        <Toast.Body className="text-white text-center">
+          {config.siteData.error.general}
+        </Toast.Body>
+      </Toast>
+    </ToastContainer>
+  );
+}
